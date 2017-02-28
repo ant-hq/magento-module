d35e6e63b6d65049afead4519cb1b3529d1c39d8
Tom Cully <tom.cully@apitalent.co.nz>
2017-02-28 20:07:29 +1300

    Initial Commit

---
+++ Observer/OrderObserver.php
@@ -0,0 +1,12 @@
+<?php
+class AntHQ_Ant_Observer_OrderObserver {
+
+    public function save($observer) {
+        //$observer contains data passed from when the event was triggered.
+        //You can use this data to manipulate the order data before it's saved.
+        //Uncomment the line below to log what is contained here:
+        //Mage::log($observer);
+
+        Mage::log('AntHQ_Ant: We just made an Observer!');
+    }
+}
---
+++ docs/events.md
@@ -0,0 +1,7 @@
+# AntHQ/Ant - Magento Events
+
+  - product create, update, delete, import (get page per page), count (get page count, and total count)
+  - order update (just for voiding)
+  - webhook (product create, update, delete) (order create, update) - needs to tell us the payment status of orders so we can ignore them if the order hasnt been paid
+
+  
