f298c2916fec4aacf756e7e28433db8923bde4bf
Tom Cully <tom.cully@apitalent.co.nz>
2017-03-01 17:44:27 +1300

    Order logging

--- Observer/OrderObserver.php
+++ Observer/OrderObserver.php
@@ -2,11 +2,6 @@
 class AntHQ_Ant_Observer_OrderObserver {
 
     public function save($observer) {
-        //$observer contains data passed from when the event was triggered.
-        //You can use this data to manipulate the order data before it's saved.
-        //Uncomment the line below to log what is contained here:
-        //Mage::log($observer);
-
-        Mage::log('AntHQ_Ant: We just made an Observer!');
+        Mage::log('AntHQ_Ant: Save Called On Order');
     }
 }
